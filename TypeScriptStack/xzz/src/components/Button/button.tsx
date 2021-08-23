93e31d614a91e861155a38b7162855e1dee7c6cd
onelazycat <onelazycat@163.com>
2021-08-23 17:03:43 +0800

    feat: 使用verdaccio搭建私有npm服务器完成、添加《深入解析css》源码

---
+++ TypeScriptStack/xzz/src/components/Button/button.tsx
@@ -0,0 +1,52 @@
+import React, { AnchorHTMLAttributes, ButtonHTMLAttributes, FC } from "react";
+import classNames from 'classnames'
+
+
+export type ButtonSize = "lg" | "sm"
+export type ButtonType = "default" | "primary" | "danger" | "link"
+
+interface BaseButtonProps {
+    className?: string;
+    disabled?: boolean;
+    btnType?: ButtonType,
+    size?: ButtonSize;
+    children: React.ReactNode;
+    href?: string
+}
+
+type NativeButtonProps = BaseButtonProps & ButtonHTMLAttributes<HTMLElement>
+
+type AnchorButtonProps = BaseButtonProps & AnchorHTMLAttributes<HTMLElement>
+
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
+
+export const Button: FC<ButtonProps> = (props) => {
+    const { btnType, className, disabled, size, children, href, ...restProps } = props
+
+    const classes = classNames('btn', className, {
+        [`btn-${btnType}`]: btnType,
+        [`btn-${size}`]: size,
+        'disabled': (btnType === 'link') && disabled
+    })
+
+    if(btnType === 'link' && href){
+        return(
+            <a className={classes} href={href} {...restProps}>
+                {children}
+            </a>
+        )
+    }else{
+        return (
+            <button className={classes} disabled={disabled} {...restProps}>
+                {children}
+            </button>
+        )
+    }
+}
+
+Button.defaultProps = {
+    disabled: false,
+    btnType: 'default'
+}
+
+export default Button
